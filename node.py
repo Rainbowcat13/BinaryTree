3d38cf3d4dd3dbd4f95cb5cceda1583bd65e2544
Rainbowcat13 <rainbowcat1308@gmail.com>
2021-04-06 21:34:58 +0300

    first version of splay tree

---
+++ node.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.height = 0
+        self.parent = None
+        self.left = None
+        self.right = None
+
+    def __eq__(self, other):
+        return self is other
+
+    def __lt__(self, other):
+        return self.key < other.key
+
+
+def set_parent(v1, v2):
+    if v1 is not None:
+        v1.parent = v2
--- splay_tree.py
+++ splay_tree.py
@@ -1,2 +1,124 @@
+from typing import Union
+
+from node import *
+
+
+def keep_parents(v: Node) -> None:
+    if v is None:
+        return
+    set_parent(v.left, v)
+    set_parent(v.right, v)
+
+
+def rotate(p: Node, v: Node) -> None:
+    g = p.parent
+    if g is not None:
+        if g.left == p:
+            g.left = v
+        else:
+            g.right = v
+    set_parent(v, g)
+    if p.left == v:
+        p.left = v.right
+        set_parent(v.right, p)
+        v.right = p
+        set_parent(p, v)
+    else:
+        p.right = v.left
+        set_parent(v.left, p)
+        v.left = p
+        set_parent(p, v)
+    v.parent = g
+
+
 class SplayTree:
-    pass
+    def __init__(self):
+        self.root = None
+
+    def _recount_heights(self, v: Node) -> int:
+        if v is None:
+            return 0
+        v.height = max(self._recount_heights(v.left),
+                       self._recount_heights(v.right)) + 1
+        return v.height
+
+    def _splay(self, v: Node) -> Node:
+        if v.parent is None:
+            return v
+        p = v.parent
+        g = p.parent
+        if g is None:
+            rotate(p, v)
+            return v
+        else:
+            zigzig = (g.left == p) == (p.left == v)
+            if zigzig:
+                rotate(g, p)
+                rotate(p, v)
+            else:
+                rotate(p, v)
+                rotate(g, v)
+            return self._splay(v)
+
+    def find(self, key) -> Node:
+        return self._find_node(self.root, key)
+
+    def _find_node(self, v, key) -> Union[Node, None]:
+        if v is None:
+            return None
+        if key == v.key:
+            return self._splay(v)
+        if key < v.key and v.left is not None:
+            return self._find_node(v.left, key)
+        if key > v.key and v.right is not None:
+            return self._find_node(v.left, key)
+        return self._splay(v)
+
+    def _split(self, v: Node, key: int) \
+            -> tuple[Union[Node, None], Union[Node, None]]:
+        if v is None:
+            return None, None
+        cur = self._find_node(v, key)
+        if cur.key == key:
+            set_parent(cur.left, None)
+            set_parent(cur.right, None)
+            return cur.left, cur.right
+        elif cur.key < key:
+            right = cur.right
+            cur.right = None
+            set_parent(right, None)
+            return cur, right
+        else:
+            left = cur.left
+            cur.left = None
+            set_parent(left, None)
+            return left, cur
+
+    def insert(self, key):
+        parts = self._split(self.root, key)
+        new_root = Node(key)
+        new_root.left = parts[0]
+        new_root.right = parts[1]
+        keep_parents(new_root)
+        tree = new_root
+        self._recount_heights(tree)
+
+    def _merge(self, left, right):
+        if right is None:
+            return left
+        if left is None:
+            return right
+        right = self._find_node(right, left.key)
+        right.left = left
+        left.parent = right
+        return right
+
+    def remove(self, key):
+        v = self._find_node(self.root, key)
+        set_parent(v.left, None)
+        set_parent(v.right, None)
+        self.root = self._merge(v.left, v.right)
+        self._recount_heights(self.root)
+
+    def clear(self):
+        self.root = None
